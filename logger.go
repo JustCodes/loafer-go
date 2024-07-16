8ead0b4a740e1b3af726790b14defd4703895326
silvio.leite <silvio.leite@olist.com>
2024-07-16 18:12:06 -0300

    Add a complete logger type and logfunc

---
+++ logger.go
@@ -0,0 +1,45 @@
+package loafergo
+
+import (
+	"log"
+	"os"
+)
+
+// A Logger is a minimalistic interface for the loafer to log messages to. Should
+// be used to provide custom logging writers for the loafer to use.
+type Logger interface {
+	Log(...interface{})
+}
+
+// A LoggerFunc is a convenience type to convert a function taking a variadic
+// list of arguments and wrap it so the Logger interface can be used.
+//
+// Example:
+//
+//	loafergo.NewManager(loafergo.Config{Logger: loafergo.LoggerFunc(func(args ...interface{}) {
+//	    fmt.Fprintln(os.Stdout, args...)
+//	})})
+type LoggerFunc func(...interface{})
+
+// Log calls the wrapped function with the arguments provided
+func (f LoggerFunc) Log(args ...interface{}) {
+	f(args...)
+}
+
+// newDefaultLogger returns a Logger which will write log messages to stdout, and
+// use same formatting runes as the stdlib log.Logger
+func newDefaultLogger() Logger {
+	return &defaultLogger{
+		logger: log.New(os.Stdout, "", log.LstdFlags),
+	}
+}
+
+// A defaultLogger provides a minimalistic logger satisfying the Logger interface.
+type defaultLogger struct {
+	logger *log.Logger
+}
+
+// Log logs the parameters to the stdlib logger. See log.Println.
+func (l defaultLogger) Log(args ...interface{}) {
+	l.logger.Println(args...)
+}
